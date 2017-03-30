fee4946451d33dd9038ed69f8036922c3c5d1c79
SandraR <srodrig284@gmail.com>
2017-03-29 22:25:57 -0400

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'uNL4mRdGy4gKXsu06rMgAvtqz',
+  consumer_secret: 'TQu2I1wHPbUSsL2hj9MbXirAE9gJzOj9c5koP39RKlpiLyGELk',
+  access_token_key: '24706078-cXAthyPTTnpYObWggr3RqoA1orecbsvr5pyVEW22C',
+  access_token_secret: '1nRNx8jAaXcaRGZL1OsPCxPCbiSHc2jErQbT3y0d3CWD9',
+}
