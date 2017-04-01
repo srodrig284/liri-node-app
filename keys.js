76dbc0967458944a6710588b3c70c18de047c515
SandraR <srodrig284@gmail.com>
2017-03-31 22:03:34 -0400

    Twitter API Working

--- keys.js
+++ keys.js
@@ -1,8 +1,10 @@
 console.log('this is loaded');
 
-exports.twitterKeys = {
+var twitter = require('twitter');
+
+exports.twitterKeys = new twitter({
   consumer_key: 'uNL4mRdGy4gKXsu06rMgAvtqz',
   consumer_secret: 'TQu2I1wHPbUSsL2hj9MbXirAE9gJzOj9c5koP39RKlpiLyGELk',
   access_token_key: '24706078-cXAthyPTTnpYObWggr3RqoA1orecbsvr5pyVEW22C',
-  access_token_secret: '1nRNx8jAaXcaRGZL1OsPCxPCbiSHc2jErQbT3y0d3CWD9',
-}
+  access_token_secret: '1nRNx8jAaXcaRGZL1OsPCxPCbiSHc2jErQbT3y0d3CWD9'
+})
--- liri.js
+++ liri.js
@@ -2,7 +2,11 @@
 // Grab the keys variable
 var myKeys = require("./keys.js");
 
+// Gets all of my twitter keys
 var client = myKeys.twitterKeys;
+
+// As always, we grab the fs package to handle read/write
+var fs = require("fs");
 
 // Parses the command line argument to capture the "operand" (deposits, withdrawals, or lotto purchases) and the numbers
 var user_request = process.argv[2];
@@ -32,10 +36,40 @@ switch (user_request)
 };
 
 function getMyTweets(){
-    var params = {screen_name: 'sandrar0218'};
+    var params = {screen_name: 'realDonaldTrump'};
     client.get('statuses/user_timeline',params, function(error, tweets, response) {
         if(error) throw error;
-        console.log(tweets);  // The favorites.
+        //console.log(tweets);  // display tweets
+
+        fs.appendFile("twitter.txt", tweets[0].user.name + "'s latest tweets\n\n", function (err) {
+            // If there was an error, we log it and return immediately
+            if (err) {
+                return console.log(err);
+            }
+        });
+
+        for (i = 0; i < tweets.length; i++)
+        {
+            fs.appendFile("twitter.txt", "Tweet Date: " + tweets[i].created_at + "\n" + tweets[i].text + "\n\n", function (err)
+            {
+                // If there was an error, we log it and return immediately
+                if (err)
+                {
+                    return console.log(err);
+                }
+                console.log("tweets array length: ",tweets.length);
+
+                console.log(tweets);  // display tweets
+            });
+        };
+
+        fs.appendFile("twitter.txt", "\n\n", function (err)
+        {
+            // If there was an error, we log it and return immediately
+            if (err)
+            {
+                return console.log(err);
+            }
+        });
     });
-    
 }
